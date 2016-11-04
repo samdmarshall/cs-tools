8fb285ca4be54464dbc92917ee6c217e1b2e4543
Samantha Marshall <me@samdmarshall.com>
2016-11-03 21:43:54 -0400

    updating code

---
+++ vaccine/vaccine.m
@@ -0,0 +1,64 @@
+/*
+Copyright (c) 2016, Samantha Marshall
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
+
+1. Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+
+2. Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
+
+3. Neither the name of Samantha Marshall nor the names of its contributors may be used to endorse or promote products derived from this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+
+#import <Foundation/Foundation.h>
+#import <iso646.h>
+
+void unset_quarantine_on_file(NSURL *file_url) {
+	NSError *resource_error = nil;
+	bool was_successful = [file_url setResourceValue:nil forKey:NSURLQuarantinePropertiesKey error:&resource_error];
+	if (not was_successful and resource_error) {
+		printf("%s\n", [[resource_error localizedDescription] UTF8String]);
+	}
+}
+
+void usage(void) {
+	printf("Overview: Allows users to remove the quarantine flag set on files or directories.\n");
+	printf("\n");
+	printf("Usage: %s [path]\n", getprogname());
+}
+
+int main(int argc, char *argv[]) {
+	int exit_code = 0;
+	@autoreleasepool {
+		NSArray *arguments = [[NSProcessInfo processInfo] arguments];
+		NSString *file_path = [arguments lastObject];
+		NSURL *requested_path = [NSURL fileURLWithPath:file_path];
+		NSFileManager *file_manager = [[NSFileManager alloc] init];
+		BOOL is_directory = NO;
+		bool exists = [file_manager fileExistsAtPath:file_path isDirectory:&is_directory];
+		bool valid_command = ([arguments count] == 2 and exists);
+		if (valid_command) {
+			if (is_directory) {
+				NSDirectoryEnumerator *directory_enumerator = [file_manager enumeratorAtPath:file_path];
+				for (NSString *path in directory_enumerator) {
+					NSURL *found_path = [NSURL fileURLWithPathComponents:@[file_path, path]];
+					BOOL path_is_directory = NO;
+					bool path_exists = [file_manager fileExistsAtPath:[found_path absoluteString] isDirectory:&path_is_directory];
+					if (path_exists and not path_is_directory) {
+						unset_quarantine_on_file(found_path);
+					}
+				}
+			}
+			else {
+				unset_quarantine_on_file(requested_path);
+			}
+		}
+		else {
+			usage();
+		}
+	}
+	return exit_code;
+}
